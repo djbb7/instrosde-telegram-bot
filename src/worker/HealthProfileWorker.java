4e5b0f799b2f3f993fadb680d98daeda9dd0dc67
djbb7 <danielbruzual@gmail.com>
2015-11-27 21:00:41 +0100

    Test numeric keyboard

--- src/worker/HealthProfileWorker.java
+++ src/worker/HealthProfileWorker.java
@@ -1,6 +1,7 @@
 package worker;
 
 import healthprofilebot.model.IdMatch;
+import healthprofilebot.model.LastCommand;
 import healthprofilebot.model.Measurement;
 import healthprofilebot.model.Person;
 import healthprofilebot.model.ServerResponse;
@@ -24,30 +25,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class HealthProfileWorker implements Runnable{
 
 	private TelegramUpdate job;
-	
+
 	private static WebTarget hpService, telegramService;
-	
-	
+
+
 	private static String telegramServer = "https://api.telegram.org/bot178041124:AAFe4zG7TpdBtrImZrv_6hNOUgYUNI4VSNQ";
-	
+
 	private static String healthProfileServer = "https://damp-cliffs-5416.herokuapp.com/rest";
-	
+
 	public HealthProfileWorker(TelegramUpdate job){
 		this.job = job;
-		
+
 		ClientConfig clientConfig = new ClientConfig();
 		//Long timeout in case heroku server is sleeping
 		clientConfig.property(ClientProperties.CONNECT_TIMEOUT, 60*1000);
 		Client client = ClientBuilder.newClient(clientConfig);
-		
+
 		//setup health profile server
 		hpService = client.target(healthProfileServer);
-		
+
 		//setup telegram server
 		telegramService = client.target(telegramServer);
 		System.out.println("[slave] initialized");
 	}
-	
+
 	@Override
 	public void run() {
 		System.out.println("[slave] Running task");
@@ -55,20 +56,21 @@ public class HealthProfileWorker implements Runnable{
 		String message = job.message.text.trim();
 		String[] parts = message.split(" ");
 		String command = parts[0];
-		
+		boolean hasErrors = false;
+
 		ServerResponse tResponse = new ServerResponse(job);
-		
+
 		System.out.println("[slave] checking db...");
 
 		//check user id in database
 		IdMatch match = IdMatch.getIdMatchByTelegramUserId(job.message.from.id);
-		
-		
+		LastCommand lastCmd = LastCommand.getLastCommand(job.message.from.id);
+
 		if(match == null && command.equals("/start")){			
 			//Let user now what is happening
 			tResponse.setText("Creating your health profile :). Please hold on.");
 			sendRequest(telegramService, "/sendMessage", "POST", tResponse);
-			
+
 			//create remote health profile
 			Person chuck = new Person();
 			chuck.setFirstname(job.message.from.first_name);
@@ -76,20 +78,20 @@ public class HealthProfileWorker implements Runnable{
 			//chuck.setEmail("");
 			//chuck.measure = new ArrayList<Measurement>();
 			//chuck.setBirthdate(new Date(2000, 4, 12));
-			
+
 			Response r = sendRequest(hpService, "/person", "POST", chuck); 
 			System.out.println("[slave]"+r.getStatus());
 			System.out.println("[slave]"+r.readEntity(String.class));
-			
+
 			if(r.getStatus() == 201){
 				Person p = r.readEntity(Person.class);
-				
+
 				//store id in local database
 				match = new IdMatch();
 				match.setTelegramUserId(job.message.from.id);
 				match.setHealthProfileId(p.getId());
 				IdMatch.saveIdMatch(match);
-			
+
 				//Report profile created
 				tResponse.setText("Done! Check out '/help' to see a list of available commands.");
 			} else {
@@ -103,7 +105,7 @@ public class HealthProfileWorker implements Runnable{
 			tResponse.setText("Welcome back, "+job.message.from.first_name);
 			sendRequest(telegramService, "/sendMessage", "POST", tResponse);
 		} else if (command.equals("/weight") || command.equals("/height") || command.equals("/blood")){
-			boolean hasErrors = false;
+			/*boolean hasErrors = false;
 			if(parts.length < 2){
 				tResponse.setText("Be sure tu include the command name followed by the measurement value. Like this: \n"+
 								  command+" 67");
@@ -116,7 +118,7 @@ public class HealthProfileWorker implements Runnable{
 								  command+" 68");
 				hasErrors = true;
 			}
-			
+
 			if(!hasErrors){
 				String path = "/person/"+match.getHealthProfileId()+command;
 				Measurement measure = new Measurement();
@@ -126,22 +128,52 @@ public class HealthProfileWorker implements Runnable{
 				Response r = sendRequest(hpService, path, "POST", measure);
 				System.out.println("[slave]"+r.getStatus());
 				System.out.println("[slave]"+r.readEntity(String.class));
-				
+
 				if(r.getStatus() == 200){
 					tResponse.setText("Great, your measurement was stored. Keep up the good work :)");
 				} else {
 					tResponse.setText("I could not save your measurement. Could you try again?");
 				}
-			}
+			}*/
+			//save command received
+			tResponse.setText("Please enter the measurement value.");
 			sendRequest(telegramService, "/sendMessage", "POST", tResponse);
-	 } else if (command.equals("/weightHistory") || command.equals("/heightHistory") || command.equals("/bloodHistory")){ 
-		
-	 } else {
+		} else if (command.equals("/weightHistory") || command.equals("/heightHistory") || command.equals("/bloodHistory")){ 
+
+		} else if (lastCmd.equals("/weight") || lastCmd.equals("/height") || lastCmd.equals("/blood")){
+			try {
+				//save measurement
+				double value = Double.parseDouble(command);
+				String path = "/person/"+match.getHealthProfileId()+command;
+				Measurement measure = new Measurement();
+				measure.value = command;
+				if(measure.value.equals("blood")) //rename to ACTUAL name on Web Server.
+					measure.value = "bloodpressure";
+				Response r = sendRequest(hpService, path, "POST", measure);
+				System.out.println("[slave]"+r.getStatus());
+				System.out.println("[slave]"+r.readEntity(String.class));
+
+				if(r.getStatus() == 200){
+					tResponse.setText("Great, your measurement was stored. Keep up the good work :)");
+				} else {
+					tResponse.setText("I could not save your measurement. Could you try again?");
+				}
+			} catch (NumberFormatException e) {
+				tResponse.setText("Are you trying to hack me :(? Please type a numeric value.");
+				sendRequest(telegramService, "/sendMessage", "POST", tResponse);
+				hasErrors = true;
+			}
+		} else {
 			//Error
 			tResponse.setText("I don't understand this command");
 			sendRequest(telegramService, "/sendMessage", "POST", tResponse);
 		}
-    	
+		
+		if(!hasErrors){
+			//update command database for user
+			LastCommand cmd = new LastCommand(job.message.from.id, command);
+			LastCommand.updateLastCommand(cmd);
+		}
 	}
 
 	private static Response sendRequest(WebTarget target, String URIPath,  String method, Object requestBody){
@@ -150,7 +182,7 @@ public class HealthProfileWorker implements Runnable{
 				.request()
 				.accept(MediaType.APPLICATION_JSON)
 				.header("Content-Type", MediaType.APPLICATION_JSON);
-		
+
 		Entity<Object> body = null;
 		if(requestBody != null){
 			body = Entity.json(requestBody);
@@ -163,7 +195,7 @@ public class HealthProfileWorker implements Runnable{
 				System.out.println("Error processing JSON object");
 			}
 		}
-		
+
 		if(method.equals("GET")){
 			response = builder.get();
 		} else if(method.equals("POST")){
@@ -175,7 +207,7 @@ public class HealthProfileWorker implements Runnable{
 		} else {
 			throw new RuntimeException("Unexpected HTTP method: "+method);
 		}
-		
+
 		response.bufferEntity();
 		System.out.println("[slave][request]["+response.getStatus()+"] "+response.readEntity(String.class));
 		return response;
